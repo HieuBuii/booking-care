c172efab6c63c849ad7705cdb01ab9316f7d154f
Hieu Bui <hieusevenxb@gmail.com>
2022-10-16 16:41:08 +0700

    push crud nodeJs

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -4,9 +4,16 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("Users", [
       {
+        email: "example@example.com",
+        password: "12345",
         firstName: "John",
         lastName: "Doe",
-        email: "example@example.com",
+        address: "Ha Noi",
+        phonenumber: "012345678",
+        gender: 1,
+        image: "DataTypes.STRING",
+        roleId: "R1",
+        positionId: "S2",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
